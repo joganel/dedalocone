b1300acca404ba984e4cce1202ba66f2729e09f1
dedaloc2 <joganel@hotmail.com>
2018-01-30 22:56:16 -0600

    On branch master  Changes to be committed: 	new file:   app/src/debug/res/values/google_maps_api.xml 	new file:   app/src/main/java/com/example/dedaloc2/camerat/view/DetailListNumberActivity.java 	new file:   app/src/main/java/com/example/dedaloc2/camerat/view/Fragments/DetailsFragment.java 	new file:   app/src/main/java/com/example/dedaloc2/camerat/view/MapsActivity.java 	new file:   app/src/main/java/com/example/dedaloc2/camerat/view/PortraitLandscapeActivity.java 	new file:   app/src/main/res/layout/activity_detail_list_number.xml 	new file:   app/src/main/res/layout/activity_maps.xml 	new file:   app/src/main/res/layout/activity_portrait_landscape.xml 	new file:   app/src/main/res/layout/fragment_details.xml 	new file:   app/src/main/res/layout/portrait_landscape_content.xml 	new file:   app/src/release/res/values/google_maps_api.xml
     Changes not staged for commit:
    	deleted:    README.md
    	modified:   app/build.gradle
    	modified:   app/src/debug/res/values/google_maps_api.xml
    	modified:   app/src/main/AndroidManifest.xml
    	modified:   app/src/main/java/com/example/dedaloc2/camerat/MainActivity.java
    	modified:   app/src/main/java/com/example/dedaloc2/camerat/view/DetailBookActivity.java
    	modified:   app/src/main/java/com/example/dedaloc2/camerat/view/DetailListNumberActivity.java
    	modified:   app/src/main/java/com/example/dedaloc2/camerat/view/Fragments/DetailsFragment.java
    	modified:   app/src/main/java/com/example/dedaloc2/camerat/view/MapsActivity.java
    	modified:   app/src/main/java/com/example/dedaloc2/camerat/view/PortraitLandscapeActivity.java
    	modified:   app/src/main/res/layout/activity_detail_list_number.xml
    	modified:   app/src/main/res/layout/activity_main.xml
    	modified:   app/src/main/res/layout/activity_portrait_landscape.xml
    	modified:   app/src/main/res/layout/fragment_details.xml
    	modified:   app/src/main/res/layout/portrait_landscape_content.xml
    	modified:   app/src/main/res/values/strings.xml
    	modified:   app/src/main/res/values/styles.xml
    	modified:   app/src/release/res/values/google_maps_api.xml
    	modified:   build.gradle

---
+++ app/src/main/java/com/example/dedaloc2/camerat/view/DetailListNumberActivity.java
@@ -0,0 +1,15 @@
+package com.example.dedaloc2.camerat.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.dedaloc2.camerat.R;
+
+public class DetailListNumberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_list_number);
+    }
+}
---
+++ app/src/main/java/com/example/dedaloc2/camerat/view/PortraitLandscapeActivity.java
@@ -0,0 +1,15 @@
+package com.example.dedaloc2.camerat.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.dedaloc2.camerat.R;
+
+public class PortraitLandscapeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_portrait_landscape);
+    }
+}
